f7e7ec84ad0fc314f9381eb114982d8dd3a6e41d
Paul Wannenmacher <paul@wannenmacher.org>
2025-09-28 12:01:22 +0200

    Translation into English

--- config/ftp.go
+++ config/ftp.go
@@ -4,5 +4,5 @@ type FTPConfig struct {
 	Host     string `yaml:"host"`
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
-	Port     int    `yaml:"port"` // Optional, Standard 21 für FTP, 22 für SFTP
+	Port     int    `yaml:"port"` // Optional, default 21 for FTP, 22 for SFTP
 }
